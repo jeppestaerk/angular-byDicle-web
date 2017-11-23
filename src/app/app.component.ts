ab748db786fe4d465663a0f1c557302b460b78ed
Jeppe Stærk <jeppe@staerk.io>
2017-11-23 19:09:20 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { MnFullpageOptions, MnFullpageService } from 'ngx-fullpage';
 
 @Component({
   selector: 'app-root',
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  @Input() public options: MnFullpageOptions = new MnFullpageOptions({
+    controlArrows: false,
+    scrollingSpeed: 1000,
+    navigation: true,
+    keyboardScrolling: true,
+    css3: true
+  });
+
+  constructor(public fullpageService: MnFullpageService) {
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MnFullpageModule } from 'ngx-fullpage';
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MnFullpageModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
